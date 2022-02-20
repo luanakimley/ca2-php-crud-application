7ae2083ff21459b14531fe65f2c6236799fd0996
Luana Kimley <luanakimley@gmail.com>
2022-02-20 20:50:43 +0000

    changes to error page

--- error.php
+++ error.php
@@ -1,23 +1,63 @@
-<!DOCTYPE html>
-<html>
 
-<!-- the head section -->
-<head>
-    <title>My Guitar Shop</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
-</head>
+<?php
+include('includes/header.php');
+?>
 
-<!-- the body section -->
-<body>
-    <header><h1>My Guitar Shop</h1></header>
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar" >
 
-    <main>
-        <h2 class="top">Error</h2>
-        <p><?php echo $error; ?></p>
-    </main>
+    <!-- Sidebar - Categories -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fa-solid fa-icons"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Categories</div>
+    </a>
 
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
-    </footer>
-</body>
-</html>
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0" />
+
+
+        <li class="nav-item active">
+            <a class="nav-link collapsed" href="index.php">
+                <i class="fa-solid fa-arrow-left"></i>
+                <span>Back to Homepage</span>
+            </a>
+        </li>
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+</ul>
+<!-- End of Sidebar -->
+
+    <div id="content-wrapper" class="d-flex flex-column">
+
+    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+    <!-- Sidebar Toggle (Topbar) -->
+    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+        <i class="fa fa-bars"></i>
+    </button>
+    <h1 class="ml-2">Expense Tracker</h1>
+</nav>
+
+    <div class="  mb-4">
+    <div class="container-fluid">
+
+    <!-- 404 Error Text -->
+    <div class="no-transactions-found">
+        <div class="error mx-auto position-relative" data-text="Error">Error</div>
+        <p class="lead text-gray-800 mb-5 mt-3 "><?php echo $error ?></p>
+       
+    </div>
+
+
+    </div>
+
+    </div>
+    
+   
+    <?php
+include('includes/footer.php');
+?>
